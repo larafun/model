a17a6c9460f50fb1519ca835c553feb95c04f8b9
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2018-11-28 21:29:53 +0300

    removed commented code

--- src/Traits/RealScopes.php
+++ src/Traits/RealScopes.php
@@ -7,24 +7,8 @@ use Larafun\Model\Builder;
 
 trait RealScopes
 {
-
     public function newEloquentBuilder($query)
     {
         return new Builder($query);
     }
-
-    // public function __call($method, $parameters)
-    // {
-    //     $name = 'realScope' . ucfirst($method);
-    //     if (method_exists($this, $name)) {
-    //         foreach ($parameters as $value) {
-    //             if (! $value) {
-    //                 return $this->query();
-    //             }
-    //         }
-    //         array_unshift($parameters, $this->query());
-    //         return call_user_func_array([$this, $name], $parameters);
-    //     }
-    //     return parent::__call($method, $parameters);
-    // }
 }
